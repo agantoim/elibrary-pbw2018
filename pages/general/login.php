3c5cd64771ecf9bedb0e16a6737b06049c94e6d7
Qolbi Fathurrohim <agankolbi@gmail.com>
2018-04-30 10:25:04 +0700

    SignUp.php and Login.php
    done

--- pages/general/login.php
+++ pages/general/login.php
@@ -1,6 +1,9 @@
 <?php  ?>
 <!DOCTYPE html>
 <html>
+	<style>
+		
+	</style>
 	<head>
 		<title>eLibrary</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,5 +14,25 @@
 	</head>
 	<body>
 		<!-- CONTENT -->
+		<div id='banner' class='container w3-card-4 w3-display-topmiddle' style='width: 98%; margin-top: 1%'>
+			<img class='' src="\..\elibrary\elibrary-pbw2018\img\banner2.jpg" alt="library" style='width: 100%'>
+			<div class='centered'>
+				<h1 class='bannerFont'>eLIBRARY</h1>
+			</div>
+		</div>
+		<div id='form' class="w3-display-topmiddle w3-container w3-card-4 w3-light-grey ">
+			<form class="">
+			  <h2>Login</h2>
+			  <p>
+			  <input id="iUsername" class="w3-input w3-border" name="first" type="text" placeholder="Username"></p>
+			  
+			  <p>
+			  <input id="iPassword" class="w3-input w3-border" name="last" type="Password" placeholder="Password"></p>
+			  <p>
+			  <button id="iLogin" class="w3-btn w3-black">Login</button>
+			  <button id="iCancel" class="w3-btn w3-black">Cancel</button></p>
+			  
+			</form>
+		</div>
 	</body>
 </html>
--- pages/general/signup.php
+++ pages/general/signup.php
@@ -1,6 +1,9 @@
 <?php  ?>
 <!DOCTYPE html>
 <html>
+	<style>
+		
+	</style>
 	<head>
 		<title>eLibrary</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,5 +14,37 @@
 	</head>
 	<body>
 		<!-- CONTENT -->
+		<div id='banner' class='container w3-card-4 w3-display-topmiddle' style='width: 98%; margin-top: 1%'>
+			<img class='' src="\..\elibrary\elibrary-pbw2018\img\banner2.jpg" alt="library" style='width: 100%'>
+			<div class='centered'>
+				<h1 class='bannerFont'>eLIBRARY</h1>
+			</div>
+		</div>
+		<div id='form' class="w3-display-topmiddle w3-container w3-card-4 w3-light-grey ">
+			<form class="">
+			  <h2>Create New Account</h2>
+			  <p>
+			  <input id="iUsername" class="w3-input w3-border" name="first" type="text" placeholder="Username"></p>
+			  
+			  <p>
+			  <input id="iPassword" class="w3-input w3-border" name="last" type="password" placeholder="Password"></p>
+			  
+			  <p>
+			  <input id="iConfirmPass" class="w3-input w3-border" name="first" type="password" placeholder="Confirm Password"></p>
+			 
+			  <p>
+			  <input id="iName" class="w3-input w3-border" name="first" type="text" placeholder="Name"></p>
+			  
+			  <p>
+			  <input id="iPhone" class="w3-input w3-border" name="first" type="text" placeholder="Phone"></p>
+			  
+			  <p>
+			  <input id="iAddress" class="w3-input w3-border" name="first" type="text" placeholder="Address"></p>
+			  <p>
+			  <button id="iRegister" class="w3-btn w3-black">Register</button>
+			  <button id="iCancel" class="w3-btn w3-black">Cancel</button></p>
+			  
+			</form>
+		</div>
 	</body>
 </html>
